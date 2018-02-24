660db815707370ba897c05e6292811f0814f1cd0
igor <bujhm1313@gmail.com>
2018-02-24 17:29:27 +0200

    [ADD] Add mailing module stub

---
+++ custom/auto_mail/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': "Auto mail",
+
+    'summary': """
+        Module to automatically parse and answer incoming emails
+    """,
+
+    'description': """
+        Module to automatically parse and answer incoming emails
+    """,
+
+    'author': 'Igor Kurilko',
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': [
+        'base',
+        'crm',
+        'mass_mailing',
+    ],
+
+    'data': [
+        # 'security/ir.model.access.csv',
+    ],
+}
