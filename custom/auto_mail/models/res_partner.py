213161727e6578d0ff90db689db40c3a1a4fa348
igor <bujhm1313@gmail.com>
2018-02-25 15:23:14 +0200

    [IMP] Add email parsing and partner creation

--- custom/auto_mail/models/res_partner.py
+++ custom/auto_mail/models/res_partner.py
@@ -1,16 +1,45 @@
 # -*- coding: utf-8 -*-
 
+from bs4 import BeautifulSoup
+
 from odoo import models, fields, api
+
+FROM = 'Igor Kurilko <bujhm1313@gmail.com>'
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    subjects = fields.Selection(
-        [('math', 'Математика'),
-         ('literature', 'Литература'),
-         ], string='Предметы')
+    subjects = fields.Char(string='Предметы')
+    school = fields.Char(string='Школа')
 
     @api.model
-    def process_message(self, *args, **kwargs):
-        pass
+    def message_new(self, msg_dict, custom_values=None):
+        if msg_dict.get('email_from') == FROM:
+            partner_values = self.msg_parse(msg_dict.get('body'))
+            res_id = self.create(partner_values).id
+        else:
+            res_id = super(ResPartner, self).message_new(
+                msg_dict, custom_values)
+
+        return res_id
+
+    @staticmethod
+    def msg_parse(msg_body):
+        soup = BeautifulSoup(msg_body)
+        text_list = map(lambda x: x.text, soup.findAll("b"))
+
+        event_name = text_list[0]
+        partner_name = text_list[1]
+        partner_email = text_list[2]
+        partner_subject = text_list[3]
+        partner_phone = text_list[4]
+        partner_school = text_list[5]
+
+        return {
+            'name': partner_name,
+            'email': partner_email,
+            'subjects': partner_subject,
+            'phone': partner_phone,
+            'school': partner_school,
+        }
